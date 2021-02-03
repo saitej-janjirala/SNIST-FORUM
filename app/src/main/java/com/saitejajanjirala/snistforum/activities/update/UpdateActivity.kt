81ff1f147782c81f330d46da10be4a6259b4c772
janji <janjiralasai420@gmail.com>
2021-02-03 23:07:49 +0530

    updated updating actvity screen block

--- app/src/main/java/com/saitejajanjirala/snistforum/activities/update/UpdateActivity.kt
+++ app/src/main/java/com/saitejajanjirala/snistforum/activities/update/UpdateActivity.kt
@@ -8,6 +8,7 @@ import android.database.Cursor
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.OpenableColumns
 import android.util.Log
 import android.view.View
@@ -58,11 +59,11 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
         selectedDocs = ArrayList()
         fbDocumentList = ArrayList()
         documentAdapter = DocumentAdpater(
-            this,
-            selectedDocs,
-            "update",
-            fbDocumentList,
-            this
+                this,
+                selectedDocs,
+                "update",
+                fbDocumentList,
+                this
         )
         docsRecyclerview.adapter = documentAdapter
         fetchDocuments()
@@ -109,12 +110,12 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
     private fun setUpViewModel() {
         val app = application as MyApplication
         updateViewModel = ViewModelProviders.of(
-            this,
-            ViewModelProviderFactory(
-                app.getSharedPrefInstance(),
-                app.getCompositeDisposable(),
-                app.getDatabaseService()
-            )
+                this,
+                ViewModelProviderFactory(
+                        app.getSharedPrefInstance(),
+                        app.getCompositeDisposable(),
+                        app.getDatabaseService()
+                )
         ).get(UpdateViewModel::class.java)
     }
 
@@ -134,19 +135,19 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
     private fun fetchDocuments() {
         val db = FirebaseFirestore.getInstance()
         db.collection("subjects/$subjectName/documents")
-            .get()
-            .addOnSuccessListener {
-                val fbDocs = ArrayList<FirebaseDocumentFile>()
-                for (i in it.documents) {
-                    if (i.exists()) {
-                        val obj = i.toObject(FirebaseDocumentFile::class.java)
-                        if (obj != null) {
-                            fbDocs.add(obj)
+                .get()
+                .addOnSuccessListener {
+                    val fbDocs = ArrayList<FirebaseDocumentFile>()
+                    for (i in it.documents) {
+                        if (i.exists()) {
+                            val obj = i.toObject(FirebaseDocumentFile::class.java)
+                            if (obj != null) {
+                                fbDocs.add(obj)
+                            }
                         }
                     }
+                    addtoList(fbDocs)
                 }
-                addtoList(fbDocs)
-            }
     }
 
     private fun addtoList(fbdocslist: ArrayList<FirebaseDocumentFile>) {
@@ -157,7 +158,7 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
 
     private fun checkPermission(): Boolean {
         val result = ContextCompat.checkSelfPermission(
-            this@UpdateActivity, Manifest.permission.READ_EXTERNAL_STORAGE
+                this@UpdateActivity, Manifest.permission.READ_EXTERNAL_STORAGE
         )
         return result == PackageManager.PERMISSION_GRANTED
 
@@ -165,19 +166,19 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
 
     private fun requestPermission() {
         ActivityCompat.requestPermissions(
-            this@UpdateActivity,
-            arrayOf(
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ),
-            Keys.PERMISSION_REQUEST_CODE
+                this@UpdateActivity,
+                arrayOf(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ),
+                Keys.PERMISSION_REQUEST_CODE
         )
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
@@ -225,19 +226,19 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
         val cr = this.contentResolver
         val mime = cr.getType(uri)
         val cursor: Cursor? = cr.query(
-            uri, null, null, null, null, null
+                uri, null, null, null, null, null
         )
 
         cursor?.use {
             if (it.moveToFirst()) {
                 val displayName: String =
-                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                        it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
                 val mimeTypes = arrayOf(
-                    "application/vnd.ms-powerpoint",
-                    "application/pdf",
-                    "application/msword",
-                    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                        "application/vnd.ms-powerpoint",
+                        "application/pdf",
+                        "application/msword",
+                        "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
                 )
                 Log.i("metadata", "Display Name: $displayName Type:$mime")
                 val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
@@ -251,7 +252,7 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
                 Log.i("metadata", "Size: $size")
                 if (mime in mimeTypes) {
                     val document =
-                        mime?.let { it1 -> Document(displayName.split(".")[0], it1, uri) }
+                            mime?.let { it1 -> Document(displayName.split(".")[0], it1, uri) }
                     if (document != null) {
                         selectedDocs.add(document)
                     }
@@ -265,54 +266,58 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
             var count = 0
             val originalcount = selectedDocs.size
             showProgressLayout()
-            val firebaseStorage = FirebaseStorage.getInstance().reference
-            val db = FirebaseFirestore.getInstance()
-            for (document in selectedDocs) {
-                document.uri?.let { it1 ->
-                    val name = document.name
-                    val path = "uploads/$name${System.currentTimeMillis()}"
-                    val ref = firebaseStorage.child(path)
-                    ref.putFile(it1)
-                        .addOnFailureListener {
-                            count += 1
-                            updateViewModel.message.postValue("$name uploading is failed")
-                            if (count == originalcount) {
-                                setSubjectInfo()
-                            }
-                        }
-                        .addOnSuccessListener {
-                            ref.downloadUrl.addOnSuccessListener { fileurl ->
-                                val url = fileurl.toString()
-                                val docref = "${System.currentTimeMillis()}"
-                                val firebaseDocumentFile =
-                                    FirebaseDocumentFile(
-                                        subjectName,
-                                        name,
-                                        url,
-                                        document.type,
-                                        false,
-                                        docref
-                                    )
-                                count += 1
-                                db.document("subjects/$subjectName/documents/$docref")
-                                    .set(firebaseDocumentFile)
-                                    .addOnSuccessListener {
+            Handler().postDelayed({
+                val firebaseStorage = FirebaseStorage.getInstance().reference
+                val db = FirebaseFirestore.getInstance()
+                for (document in selectedDocs) {
+                    document.uri?.let { it1 ->
+                        val name = document.name
+                        val path = "uploads/$name${System.currentTimeMillis()}"
+                        val ref = firebaseStorage.child(path)
+                        ref.putFile(it1)
+                                .addOnFailureListener {
+                                    count += 1
+                                    updateViewModel.message.postValue("$name uploading is failed")
+                                    if (count == originalcount) {
+                                        setSubjectInfo()
                                     }
-                                    .addOnFailureListener { exception ->
-                                        exception.message?.let { it2 ->
-                                            updateViewModel.message.postValue(
-                                                it2
-                                            )
-                                        }
-                                    }
-                                if (count == originalcount) {
-                                    setSubjectInfo()
                                 }
+                                .addOnSuccessListener {
+                                    ref.downloadUrl.addOnSuccessListener { fileurl ->
+                                        val url = fileurl.toString()
+                                        val docref = "${System.currentTimeMillis()}"
+                                        val firebaseDocumentFile =
+                                                FirebaseDocumentFile(
+                                                        subjectName,
+                                                        name,
+                                                        url,
+                                                        document.type,
+                                                        false,
+                                                        docref
+                                                )
+                                        count += 1
+                                        db.document("subjects/$subjectName/documents/$docref")
+                                                .set(firebaseDocumentFile)
+                                                .addOnSuccessListener {
+                                                }
+                                                .addOnFailureListener { exception ->
+                                                    exception.message?.let { it2 ->
+                                                        updateViewModel.message.postValue(
+                                                                it2
+                                                        )
+                                                    }
+                                                }
+                                        if (count == originalcount) {
+                                            setSubjectInfo()
+                                        }
 
-                            }
-                        }
+                                    }
+                                }
+                    }
                 }
-            }
+
+            }, 500)
+
         }
 
     }
@@ -327,17 +332,18 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
 
     private fun setSubjectInfo() {
         progresstext.text =
-            "subject uploaded"
+                "subject uploaded"
         progressBar.visibility = View.GONE
         okayBtn.visibility = View.VISIBLE
         updateViewModel.message.postValue("subject uploaded successfully")
         selectedDocs.clear()
     }
+
     //reccyclerview onitemclicks
     override fun onItemClicked(
-        position: Int,
-        document: Document,
-        fbDocuemnt: FirebaseDocumentFile
+            position: Int,
+            document: Document,
+            fbDocuemnt: FirebaseDocumentFile
     ) {
         TODO("Not yet implemented")
     }
@@ -351,20 +357,20 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
             val map = HashMap<String, Any>()
             map["deleted"] = isDeleted
             db.document("subjects/${mDoc.subjectname}/documents/${mDoc.docRefname}")
-                .update(map)
-                .addOnSuccessListener {
-                    setDelete(isDeleted, imageView)
-                    fbDocumentList[position].isDeleted = isDeleted
-                    documentAdapter.notifyDataSetChanged()
-                    if (isDeleted) {
-                        Toaster.show(this@UpdateActivity, "marked to not show")
-                    } else {
-                        Toaster.show(this@UpdateActivity, "marked to show")
+                    .update(map)
+                    .addOnSuccessListener {
+                        setDelete(isDeleted, imageView)
+                        fbDocumentList[position].isDeleted = isDeleted
+                        documentAdapter.notifyDataSetChanged()
+                        if (isDeleted) {
+                            Toaster.show(this@UpdateActivity, "marked to not show")
+                        } else {
+                            Toaster.show(this@UpdateActivity, "marked to show")
+                        }
                     }
-                }
-                .addOnFailureListener {
-                    it.message?.let { it1 -> Toaster.show(this@UpdateActivity, it1) }
-                }
+                    .addOnFailureListener {
+                        it.message?.let { it1 -> Toaster.show(this@UpdateActivity, it1) }
+                    }
         } else {
             Toaster.show(this@UpdateActivity, getString(R.string.no_internet_connection))
         }
@@ -376,9 +382,9 @@ class UpdateActivity : AppCompatActivity(), DocumentAdpater.OnDocumentItemClickL
     }
 
     override fun onDownloadClicked(
-        position: Int,
-        fbDocument: FirebaseDocumentFile,
-        fileExists: Boolean
+            position: Int,
+            fbDocument: FirebaseDocumentFile,
+            fileExists: Boolean
     ) {
         TODO("Not yet implemented")
     }
